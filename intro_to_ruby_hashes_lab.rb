f592f9fe567464f80620506a59749e947f4dce12
Pavel Ilin <pavel_ilin@yahoo.com>
2019-08-29 21:20:58 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,17 +1,30 @@
 def new_hash
   # return an empty hash
+  new_hash = {}
 end
 
 def my_hash
   # return a valid hash with any key/value pair of your choice
+  my_hash = {
+    key: 'value',
+    key1: 5
+  }
 end
 
 def pioneer
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  pioneer = {
+    :name => 'Grace Hopper'
+  }
+  pioneer[:name]
 end
 
 def id_generator
   # return a hash with a key :id assigned to positive integer
+  id_generator = {
+    :id => 10
+  }
+  id_generator[:id]
 end
 
 def my_hash_creator(key, value)
